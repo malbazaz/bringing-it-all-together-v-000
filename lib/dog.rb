9b2d820d96f2bcc995f596e86aa727894d7c2a52
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-20 19:55:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,8 +59,8 @@ attr_accessor :name, :breed, :id
       SELECT * FROM dogs
       WHERE id = ?;
     SQL
-    found_dog = DB[:conn].execute(sql, id).map do |row|
-      self.new_from_db(row)
+    found_dog = DB[:conn].execute(sql, id)[0]
+      self.new_from_db(found_dog)
     end
     #Dog.new(found_dog[1], found_dog[2], found_dog[0])
   end
